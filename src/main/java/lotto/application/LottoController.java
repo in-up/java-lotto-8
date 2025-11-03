9fba3cbf3b5559f614ae9e4fbf01d6093ee90ea4
in-up <inup@kyonggi.ac.kr>
2025-11-03 22:13:49 +0900

    feat: 로또 구매 및 발급

--- src/main/java/lotto/application/LottoController.java
+++ src/main/java/lotto/application/LottoController.java
@@ -1,21 +1,41 @@
 package lotto.application;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
 import lotto.io.InputView;
 import lotto.io.OutputView;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final LottoMachine lottoMachine;
 
     public LottoController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.lottoMachine = new LottoMachine();
     }
 
     public void run() {
-        // TODO: 로또 게임 전체 흐름 구현
-        String purchaseAmount = inputView.readPurchaseAmount();
-        String winningNumbers = inputView.readWinningNumbers();
-        String bonusNumber = inputView.readBonusNumber();
+        List<Lotto> purchasedLottos = purchaseLottos();
+        printPurchasedLottos(purchasedLottos);
+
+        // TODO: 당첨번호 입력 및 결과처리
+    }
+
+    private List<Lotto> purchaseLottos() {
+        String input = inputView.readPurchaseAmount();
+        int amount = Integer.parseInt(input);
+        return lottoMachine.purchaseLottos(amount);
+    }
+
+    private void printPurchasedLottos(List<Lotto> lottos) {
+        outputView.printPurchaseCount(lottos.size());
+        List<List<Integer>> lottoNumbers = lottos.stream()
+                .map(Lotto::getNumbers)
+                .collect(Collectors.toList());
+        outputView.printLottos(lottoNumbers);
     }
 }
---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    private static final int LOTTO_PRICE = 1000;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    public List<Lotto> purchaseLottos(int amount) {
+        validateAmount(amount);
+        int count = calculateLottoCount(amount);
+        return generateLottos(count);
+    }
+
+    private void validateAmount(int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("구입 금액은 1,000원 단위여야 합니다.");
+        }
+    }
+
+    private int calculateLottoCount(int amount) {
+        return amount / LOTTO_PRICE;
+    }
+
+    private List<Lotto> generateLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                    MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT
+            );
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+}
